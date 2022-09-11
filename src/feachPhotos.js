357eff75bac73892c00aabe70209ed71a2917a95
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-09-11 22:18:28 +0100

    Finished axios

--- src/feachPhotos.js
+++ src/feachPhotos.js
@@ -1,16 +1,64 @@
-import { Loading } from 'notiflix';
+import axios from 'axios';
+import { Notify } from 'notiflix';
+// import axios from './axios';
 
 export { feachPhotos };
 
-function feachPhotos(searchPhotos) {
+async function feachPhotos(searchPhotos) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '29782836-0cb6e5c5167e525a8102df66c';
-  //   #searchQuery = '';
-  const url = `${BASE_URL}?key=${API_KEY}&q=${searchPhotos}&image_type=photo&orientation=horizontal&safesearch=true`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchPhotos}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`;
+  // const response = await fetch(url);
+  const response = await axios.get(
+    `https://pixabay.com/api/?key=29782836-0cb6e5c5167e525a8102df66c&q=${searchPhotos}`
+  );
+  // {
+  //   params: {
+  //     // q: 'searchPhotos',
+  //     image_type: 'photo',
+  //     orientation: 'horizontal',
+  //     safesearch: 'true',
+  //     page: 1,
+  //     per_page: 40,
+  //   },
+  //   header: {
+  //     'Content-Type': 'aplication/json',
+  //     Authorization: '29782836-0cb6e5c5167e525a8102df66c',
+  //   },
+  // }
+  // );
+  console.log(response);
+  if (!response.ok) {
+    throw new Error(response.status);
+    Notify.failure(error);
+  }
+  console.log(response);
+  // return response.json();
+  return response.data;
 }
+
+// async function feachPhotos(searchPhotos) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const API_KEY = '29782836-0cb6e5c5167e525a8102df66c';
+//   //   #searchQuery = '';
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${searchPhotos}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   const response = await fetch(url);
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//     Notify.failure(error);
+//   }
+//   return response.json();
+// }
+
+// function feachPhotos(searchPhotos) {
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const API_KEY = '29782836-0cb6e5c5167e525a8102df66c';
+//   //   #searchQuery = '';
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${searchPhotos}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   return fetch(url).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import Notiflix, { Notify } from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { feachPhotos } from './feachPhotos';
@@ -10,32 +9,41 @@ import { feachPhotos } from './feachPhotos';
 
 const formRef = document.querySelector('form#search-form');
 const divGalleryRef = document.querySelector('.gallery');
-const divPhotoRef = document.querySelector('.photo-card');
+// const divPhotoRef = document.querySelector('.photo-card');
 const submitBtnRef = document.querySelector('form button[type="submit"]');
+const galleryEl = document.querySelector('.gallery');
+const buttonBtnRef = document.querySelector('.load-more');
 // console.log(submitBtnRef);
 formRef.addEventListener('submit', onSearch);
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
   const {
     elements: { searchQuery },
   } = event.currentTarget;
   const searchPhotos = searchQuery.value;
-  console.log(searchPhotos);
+  console.log('input =>', searchPhotos);
   if (!searchQuery.value) {
     console.log('Enter data');
+    Notify.failure('Enter data');
     return;
   }
-
-  feachPhotos(searchPhotos)
-    .then(data => {
-      createPhotos(data);
-    })
-    .catch(error => {
-      console.log(error);
-      Notify.failure(error);
-    });
   event.currentTarget.reset();
+  // feachPhotos(searchPhotos)
+  //   .then(data => {
+  //     createPhotos(data);
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //     Notify.failure(error);
+  //   });
+  try {
+    const result = await feachPhotos(searchPhotos);
+    const data = await createPhotos(result);
+  } catch (error) {
+    console.error(error);
+    Notify.failure(error);
+  }
 }
 
 // pixabayApi.searchQuery = searchQuery.value;
@@ -48,7 +56,7 @@ function onSearch(event) {
 //   });
 
 function createPhotos(data) {
-  console.log(data);
+  console.log('data =>', data);
   const photos = data.hits;
   if (photos.length === 0) {
     console.log(
@@ -70,13 +78,25 @@ function createPhotos(data) {
     //   </ul>`;
     //     })
     //     .join('');
-    divPhotoRef.innerHTML = photos
-      .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-        return `<img
+    divGalleryRef.innerHTML = photos
+      .map(
+        ({
+          largeImageURL,
+          webformatURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<div class="photo-card">
+          <a class="gallery__item" href="${largeImageURL}">
+          <img class="gallery__image"
         src="${webformatURL}"
         alt="${tags}"
+        title="${tags}"
         loading="lazy"
-      />
+      /></a>
       <div class="info">
         <p class="info-item">
           <b>Likes: ${likes}</b>
@@ -90,36 +110,42 @@ function createPhotos(data) {
         <p class="info-item">
           <b>Downloads: ${downloads}</b>
         </p>
-      </div>`;
-      })
+      </div>
+    </div>`;
+        }
+      )
       .join('');
   }
 }
 
-// function renderPhotos(data) {
-//   if (data.length === 0) {
-//     console.log(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//     Notify.failure(
-//       'Sorry, there are no images matching your search query. Please try again.'
-//     );
-//     return data;
-//   }
+galleryEl.addEventListener('click', openModalImg);
 
-//   if (data.length > 0) {
-//     divGalleryRef.innerHTML =
-//   }
-// }
+function openModalImg(evt) {
+  evt.preventDefault();
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+
+  var gallery = $('.gallery a').simpleLightbox();
+
+  gallery.refresh();
+}
+
+buttonBtnRef.addEventListener('click', onPagination);
+
+function onPagination(evt) {
+  console.log('onPagination=>');
+}
 
 //**Прокручування сторінки */
 
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
+const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
 
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: 'smooth',
-// });
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
 //** */
